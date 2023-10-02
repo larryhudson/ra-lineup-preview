344baccae85e7435b2a681617284c9cc82b346d6
Larry Hudson <10627494+larryhudson@users.noreply.github.com>
2023-10-03 09:08:55 +1100

    feat: working on ra lookup and soundcloud search

--- src/utils/ra.js
+++ src/utils/ra.js
@@ -1,8 +1,43 @@
+import cheerio from "cheerio";
+
 export async function getArtistsForEvent(eventUrl) {
+  console.log("Getting artists for event with URL", eventUrl);
   // download the event page html
+  // I got the headers from Firefox's browser dev tools - right click on network request and click 'use as fetch in console'
+  const eventPageResponse = await fetch(eventUrl, {
+    credentials: "include",
+    headers: {
+      "User-Agent":
+        "Mozilla/5.0 (X11; Linux x86_64; rv:109.0) Gecko/20100101 Firefox/118.0",
+      Accept:
+        "text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,*/*;q=0.8",
+      "Accept-Language": "en-US,en;q=0.5",
+      "Upgrade-Insecure-Requests": "1",
+      "Sec-Fetch-Dest": "document",
+      "Sec-Fetch-Mode": "navigate",
+      "Sec-Fetch-Site": "cross-site",
+    },
+    method: "GET",
+    mode: "cors",
+  });
+  const eventPageHtml = await eventPageResponse.text();
+  console.log(eventPageHtml);
   // parse the html
+  const $ = cheerio.load(eventPageHtml);
   // get the ld+json script and parse it, then get the 'performer' array
+  const jsonScriptTag = $("script[type='application/ld+json']");
+  const jsonScriptTagContent = jsonScriptTag.html();
+  console.log(jsonScriptTagContent);
+  const jsonScriptTagJson = JSON.parse(jsonScriptTagContent);
+  console.log(jsonScriptTagJson);
+  const artistsArray = jsonScriptTagJson.performer;
   // return the array
+  return artistsArray.map((artist) => {
+    return {
+      name: artist.name,
+      url: artist.url,
+    };
+  });
 }
 
 export async function getSoundcloudUrlForArtist(artistUrl) {
--- src/utils/soundcloud.js
+++ src/utils/soundcloud.js
@@ -1,7 +1,7 @@
 export async function getSearchResults({ searchQuery }) {
   // set these in an environment variable?
-  const userId = "MY_USER_ID_HERE";
-  const clientId = "MY_CLIENT_ID_HERE";
+  const userId = import.meta.env.SPOTIFY_USER_ID;
+  const clientId = import.meta.env.SPOTIFY_CLIENT_ID;
   // construct the search url
   const searchUrl = new URL("https://api-v2.soundcloud.com/search/tracks");
   searchUrl.searchParams.set("q", searchQuery);
@@ -15,19 +15,28 @@ export async function getSearchResults({ searchQuery }) {
   searchUrl.searchParams.set("variant_ids", 2622); // not sure what this does
   searchUrl.searchParams.set("filter.duration", "epic");
   // do the fetch
-  const searchHeaders = {
-    Accept: "application/json, text/javascript, */*; q=0.01",
-    "Accept-Language": "en-US,en;q=0.5",
-    "Cache-Control": "no-cache",
-    Pragma: "no-cache",
-    "X-Requested-With": "XMLHttpRequest",
+
+  const fetchOptions = {
+    credentials: "omit",
+    headers: {
+      "User-Agent":
+        "Mozilla/5.0 (X11; Linux x86_64; rv:109.0) Gecko/20100101 Firefox/118.0",
+      Accept: "application/json, text/javascript, */*; q=0.01",
+      "Accept-Language": "en-US,en;q=0.5",
+      "Sec-Fetch-Dest": "empty",
+      "Sec-Fetch-Mode": "cors",
+      "Sec-Fetch-Site": "same-site",
+    },
+    referrer: "https://soundcloud.com/",
+    method: "GET",
+    mode: "cors",
   };
-  const searchResponse = await fetch(searchUrl.toString());
+  const searchResponse = await fetch(searchUrl.toString(), fetchOptions);
   const json = await searchResponse.json();
 
   const tracks = json.collection;
   // we don't need all the object keys
   return tracks.map((track) => {
-    return {};
+    return track;
   });
 }
